bf6a3db1d725d2a645659a988a353556bcb409b2
Soumya Raj Biswal <biswalsoumyaraj@gmail.com>
2021-04-21 10:10:41 +0530

    done all

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,12 +1,12 @@
 const products = [
   {
     
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    name: 'TULIPS',
+    image: '/images/tulips.jpg',
     description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
+      "Our spring season would not be the same without it; the tulip is a favourite that fills us with so much joy each year. In the Eastern language of flowers, tulips were employed as the emblem by which a lover makes his passion known, yet today these flowers are a conveyor of many messages from friendship to motherhood.Adored for their variety of colours from yellow through to deep purple, here at FLORA we celebrate three beautifully unique varieties; the classic Dutch tulip celebrating Holland’s colourful Keukenhof fields, the Double tulip with its layered frilly petals and our best of British tulips from growers in Lincolnshire. Send Mother's Day tulips today and receive complimentary standard shipping as well as gift wrapping for an unforgettable surprise delivery to their door.", 
+    brand: 'LOX',
+    category: 'Flower',
     price: 89.99,
     countInStock: 10,
     rating: 4.5,
@@ -14,68 +14,185 @@ const products = [
   },
   {
     
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    name: 'ITALIAN RANUNCULUS',
+    image: '/images/ranunculus.jpg',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 599.99,
+      "Grown in Italy with precision to provide a long-lasting and full display of densely-petalled blooms that mirror tightly wound ribbons of silk, each of our ranunculus flowers are a work of art. Boasting much larger heads than their counterparts, these flowers are available in show-stopping shades such as Chiffon, Ballet Slippers, Red Velvet and Black Raspberry, with each shade making for the perfect surprise to a loved one or as a memorable floral arrangement in your home. Make your love of flowers go further with the Chiffon Ranunculus Vase Set, including 25 stems accompanied with the styled Medium Curve Windsor Vase for a truly sophisticated pairing. Sending as a gift? We include complimentary gift wrapping on all orders.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 90,
     countInStock: 7,
     rating: 4.0,
     numReviews: 8,
   },
   {
     
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
+    name: 'ROSES',
+    image: '/images/roses.jpg',
     description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
-    price: 929.99,
+      "At FLORA, our mission is to deliver beauty and happiness through flowers and as such we are pleased to introduce our collection of roses, with over fifteen roses varieties to choose from. As well as being a symbol of love, roses are an interpreter of many other feelings, from friendship and grace. Each one of our FLORA roses has a distinguishable character, with none the same as the next. Whether you’re looking to impress your other half with Valentine’s Day red roses or you simply want to show someone close you care with our luxury roses delivery, send roses today and lift the ones you love with flowers.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 45,
     countInStock: 5,
     rating: 3,
     numReviews: 12,
   },
   {
     
-    name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
+    name: 'ROSELILLY',
+    image: '/images/roselilly.jpg',
     description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
-    category: 'Electronics',
-    price: 399.99,
+      "The magnificent Roselily offers the best of both flowers as it opens, revealing an impactful display of double lily flower heads with rose-like centres. As well as their lighter fragrance, this particular variety also produces no pollen, making them a great advantage over traditional lilies. The Roselily is guaranteed to bring wow-factor to any dining table or living room during the warmer months.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 55,
     countInStock: 11,
     rating: 5,
     numReviews: 12,
   },
   {
     
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
+    name: 'ASTILBE',
+    image: '/images/astilbe.jpg',
     description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-    brand: 'Logitech',
-    category: 'Electronics',
-    price: 49.99,
+      "Astilbe, also lovingly referred to as False Goats’ Beard, is recognisable by its large, often fern-like foliage and dense, feathery plumes of flowers. In colours such as Dove Wing, Pink Flutter, Hot Tickle and Autumn Feather, these statement stems will brighten up any environment",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 40,
     countInStock: 7,
     rating: 3.5,
     numReviews: 10,
   },
   {
     
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
+    name: 'GERBERA DAISIES',
+    image: '/images/daisies.jpg',
     description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
-    price: 29.99,
+      "The meanings of Gerbera flowers originate from those attributed to the general daisy family, such as innocence and purity. Daisies are also a classic symbol of beauty. However, the Gerbera Daisy variety holds an added, and very understandable, meaning of cheerfulness, which stems from the assortment of colours available - ranging from light to dark yellow, orange, pink, brilliant scarlet, deep red, and many more. For arrangements similar to the Gerbera, explore the Scabiosa collection for alternatives that are still packed with colour and energy.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 60,
     countInStock: 0,
     rating: 4,
     numReviews: 12,
   },
+  {
+    
+    name: 'FREESIAS',
+    image: '/images/freesias.jpg',
+    description:
+      "Another of our most popular cut flowers, the delightful freesia is a genus of herbaceous perennial flowering plants in the family Iridaceae, adored for the sensationally sweet perfume it gives off, as well as having a long vase life. Native to South Africa, freesias bear tall stems and bud in colours of white, orange, red, pink and purple, bringing a burst of fresh colour and fragrance into any room. With six key shades to choose from, our cut freesias are perfect for any occasion.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 60,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'BOUVARDIA',
+    image: '/images/bouvardia.jpg',
+    description:
+      "The delicate and more unknown flower bouvardia is perfect for those with a quieter, understated taste when it comes to flower arrangements in the home or office, boasting small star-shaped flowers edged with bright green foliage. A symbol of enthusiasm and zest for life, these cut flowers are perfect for any occasion and are available at FLORA in a variety of eye-catching colours from the simpler ‘White Butterfly’ to the fiery Red Carpet and Fireball shades.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 45,
+    countInStock: 20,
+    rating: 5,
+    numReviews: 20,
+  },
+  {
+    
+    name: 'CARNATIONS',
+    image: '/images/carnations.jpg',
+    description:
+      "As with the rose, the carnation is an indispensable flower which holds a myriad of meanings depending on the colour offered, from love and devotion to friendship. Known as the flower of the God, the carnation was originally a wild plant in Europe and known to have been used for the first time by Greeks and Romans in garlands. Today, their popularity remains instilled in the flower world, universally loved for their wide spectrum of colours, beautiful petal formation and sweet fragrance, as well as their all year-round availability so you can fill your home with scent on a continual basis. Pink carnations are also traditionally given to mothers on Mothering Sunday as a token of love and gratitude, so tell her how much you appreciate her with four shades to choose from when it comes to sending Mother’s Day flowers.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 40,
+    countInStock: 30,
+    rating: 4,
+    numReviews: 20,
+  },
+  {
+    
+    name: 'SNAPDRAGONS',
+    image: '/images/snapdragon.jpg',
+    description:
+      "The snapdragon flower, or antirrhinum as its more commonly known is a towering bloom and a symbol of strength. An English garden favourite, these flowers burst in the summer in colours of white, yellow, pink, fuchsia, red and purple with a long flowering period from June to October. It's never been simpler to send snapdragons online or to have these flowers delivered to your home for an instant mood lift wth FLORA. This much-loved flower has also been included in our Vase Set collection to take the hard work out for you when bringing flowers into the home or gifting a loved one. Simply choose your colour accompanied by FLORA's signature Apothecary Vase and we will take care of the rest, with complimentary gift wrapping and free standard shipping.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 45,
+    countInStock: 30,
+    rating: 4,
+    numReviews: 10,
+  },
+  {
+    
+    name: 'CALLA LILY',
+    image: '/images/lily.jpg',
+    description:
+      "It's no surprise that the calla lily is a symbol of both magnificence and beauty. Bearing tall stems and elegant, funnel-shaped flowers in a range of colours, from green to pink, purple, yellow and orange, they're one of the more unique lily flowers on the market, with the white calla bring the most popular choice. Combining its two symbolic attributes with the colour of purity and innocence, they’re a widely popular choice as wedding flowers for the big day and are also a token of good luck for the happy future couple. You will find these exquisite cut flowers at the beginning of spring when their foliage starts to appear, blossoming fully for the summer when these flowers are at the height of their beauty.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 45,
+    countInStock: 30,
+    rating: 4,
+    numReviews: 10,
+  },
+  {
+    
+    name: 'LISIANTHUS',
+    image: '/images/lisianthus.jpg',
+    description:
+      "This tall and delicate wildflower, also referred to as the ‘prairie gentian’ or 'Texas bluebell', takes its name from the extensive breeding Japanese growers underwent on the prairies of North America during the 1930s to bring us this herbaceous plant. Blooming from March through to September, lisianthus flowers are symbolic of gratitude and appreciation and can last for a whole week or longer, making it an enduring cut flower choice for yourself or a loved one. Shades vary from soft pink, lavender, apricot and green to the more intriguing bi-colours such as blue-violet.",
+    brand: 'LOX',
+    category: 'Flower',
+    price: 40,
+    countInStock: 30,
+    rating: 4,
+    numReviews: 12,
+  },{
+    
+    name: 'HYPERICUM',
+    image: '/images/hypericum.jpg',
+    description:
+      "Elevate your interior scheme with decorative branches as a sophisticated alternative to cut flowers; our hypericum berries are an instant stylish solution. A genus of flowering plants in the family Hypericaceae, and commonly known as St Johns wort, they start out with star-shaped flowers bursting to life in midsummer, with attractive berries developing into late summer and beyond. Both tolerant and easy to care for makes hypericum berries a popular choice, especially over the festive season with their bold berries.",
+    brand: 'LOX',
+    category: 'Plant',
+    price: 40,
+    countInStock: 30,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'GREEN CHEESEPLANT',
+    image: '/images/cheeseplant.jpg',
+    description:
+      "The tropical cheese plant, also known as Monstera deliciosa, originates from Mexico and is a common indoor plant characterised by large heart-shaped leaves with holes. Available now at FLOWERBX, our long-lasting cheese plant leaf allows you to add a minimal dash of green as a cool alternative to cut flowers. Discover the FLORA Plant Shop for more green inspiration",
+    brand: 'LOX',
+    category: 'Plant',
+    price: 40,
+    countInStock: 10,
+    rating:5,
+    numReviews: 22,
+  },
+  {
+    
+    name: 'EUCALYPTUS',
+    image: '/images/eucalyptus.jpg',
+    description:
+      "The eucalyptus is an evergreen tree or shrub widely used for its medicinal properties, from herbal tea to oils, but has become popular within contemporary living spaces as a stylish alternative to cut flowers. We believe our eucalyptus varieties are just as beautiful on their own or as part of a floral arrangement, especially during the festive season, releasing a fresh, herbal aroma with tall stems, chalky leaves and in some, the appearance of delicate berries.",
+    brand: 'LOX',
+    category: 'Plant',
+    price: 45,
+    countInStock: 10,
+    rating:5,
+    numReviews: 20,
+  },
+  
 ]
 export default products;
